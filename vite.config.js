037b12f0d3d07ea4b24833b295c08965bdf3e041
hafiz.rosman <hfzrsmn@gmail.com>
2025-06-04 10:44:41 +0800

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://jai-hafiz.github.io/etiqa-assessment/',
+  base: '/etiqa-assessment/',
   plugins: [react()],
 })
