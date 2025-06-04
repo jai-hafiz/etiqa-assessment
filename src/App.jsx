ebb09b83e89893052b2c807645d12dee61143720
hafiz.rosman <hfzrsmn@gmail.com>
2025-06-04 12:05:19 +0800

    start from root

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
-import GithubPage from './pages/githubPage';
+import React from "react";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import GithubPage from "./pages/githubPage";
 
 const App = () => {
   return (
-  <>
+    <>
       <BrowserRouter>
         <Routes>
-          <Route path="/etiqa-assessment" element={<GithubPage />} />
+          <Route path="/" element={<GithubPage />} />
         </Routes>
       </BrowserRouter>
     </>
-    )
+  );
 };
 
 export default App;
